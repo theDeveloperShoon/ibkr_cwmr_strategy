af023b6ce21b6227223bc7b787e4d372bbfa8c1b
Sean Cotter <Seancotter04@gmail.com>
2026-06-28 19:53:34 -0400

    Initial commit: Compiler Errors past me.  I welcome pandora's box

---
+++ IBKR_CMAKE_MEAN_REVERSION.cpp
@@ -0,0 +1,16 @@
+﻿// IBKR_CMAKE_MEAN_REVERSION.cpp : Defines the entry point for the application.
+//
+
+#include "IBKR_CMAKE_MEAN_REVERSION.h"
+#include "EWrapper.h"
+#include "EClientSocket.h"
+#include "EReaderSignal.h"
+#include "EReader.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
